135c6aee0d4047ae44530377e0b4e5d702f0bab8
Ân <info@zindo.info>
2018-10-29 22:53:50 +0700

    Add sidebar-right file

---
+++ sidebar-right.php
@@ -0,0 +1,11 @@
+<?php 
+if ( ! defined( 'ABSPATH' ) )
+     exit;
+
+?>
+<!-- sidebar: style can be found in sidebar.less -->
+<section class="sidebar">
+    <h3><?php echo __( 'Đang cập nhật', 'codestar_lamface' ) ?></h3>
+    <?php dynamic_sidebar( 'sidebar-1' ) ?>
+</section>
+<!-- /.sidebar -->
