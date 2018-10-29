5a5191dda189fc27e60c5b3b3d90fa5b97296ff1
tinh <tinh@wiserobot.com>
2018-10-29 20:23:23 +0700

    homepage changes: update save post part -1

--- home.php
+++ home.php
@@ -510,25 +510,142 @@ get_header();
 						</div>
 					</div>
 
-					<div class="row">
-						<div class="col-xs-12">
-							<div class="box">
-								<div class="box-header with-border">
-										<h3 class="box-title">XEM NHIỀU NHẤT 24H QUA</h3>
-										<div class="box-tools pull-right">
-											<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-											</button>
-											<button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i>
-											</button>
-										</div>
-								</div>
-								<div class="box-body">
-									<!-- DoShortcode Xem nhiều nhất 24h qua -->
-									<?php echo do_shortcode('[lf_category_view id="13" page="5" zone="most-like-zone"]'); ?>
+					<?php if(is_user_logged_in()):?>
+						<div class="row">
+							<div class="col-xs-12">
+								<div class="box">
+									<div class="box-header with-border">
+											<h3 class="box-title">Content đã lưu</h3>
+											<div class="box-tools pull-right">
+												<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+												</button>
+												<button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i>
+												</button>
+											</div>
+									</div>
+									<div class="box-body">
+										<!-- DoShortcode Xem nhiều nhất 24h qua -->
+										<?php //echo do_shortcode('[lf_category_view id="13" page="5" zone="most-like-zone"]'); ?>
+										<?php 
+										$path = $_SERVER['DOCUMENT_ROOT'] . "/tmp/" . $user_id . ".user";
+										$arr  = json_decode( file_get_contents( $path ), true );
+										if ( count( $arr) == 0 ):
+											$arr = array();
+										else:?>
+											<div class="mansory-grid"><div class="grid-sizer"></div><div class="category-post">
+											<?php foreach( $arr as $it):?>
+												<?php
+												// $additional = json_decode( $item->post_additional'] );
+												global $wpdb; $result = $wpdb->get_results( 'SELECT * FROM ' . 'ltt_ff_posts_' . trim($it['cat_id']) . ' WHERE post_id = \'' . $it['post_id'] . '\'', OBJECT );
+												$item = $result[0];
+												$interactive = json_decode($item->post_additional); 
+												?>
+												<div class="item-featured" id="<?php echo trim( $item->post_id ); ?>">
+													<div class="wrap">
+														<div class="avatar">
+															<?php
+															if ( $item->media_type == "video" ) {
+																?>
+																<img src="<?php echo $item->image_url; ?>" alt="" onclick="clickToShow('<?php echo $item->post_id; ?>');return false;">
+																<img class='pview_video' src="<?php echo SITE_URL; ?>wp-content/themes/flatsome/bulkff/assets/images/play.png">
+																<?php
+															} else {
+																if ( ! empty( $item->image_url ) ) {
+																	?>
+																	<div class="thumb-img">
+																		<img src="<?php echo $item->image_url; ?>" alt="" onclick="clickToShow('<?php echo $item->post_id; ?>');return false;">
+																		<?php
+																		$medias = explode( ",", $item->medias );
+																		if ( count( $medias ) > 1 && false) {
+																			?>
+																			<div class="slider">
+																				<div class="owl-carousel owl-theme">
+																					<?php
+																					foreach ( $medias as $img ) {
+																						?>
+																						<a><img aconclick="im_img(this);" src='<?php echo $img; ?>'></a>
+																						<?php
+																					}
+																					?>
+																				</div>
+																			</div>
+																			<?php
+																		}
+																		?>
+																	</div>
+																	<?php
+																} else {
+																	?>
+																	<div class="thumb-img">
+																		<img src="/wp-content/themes/flatsome/assets/img/placeholder-image.jpg" alt="" onclick="clickToShow('<?php echo $item->post_id; ?>');return false;">
+																	</div>
+																	<?php
+																}
+															}
+															?>
+														</div>
+														<div class="desc" onclick="clickToShow('<?php echo $item->post_id; ?>', <?php echo trim($it['cat_id']); ?>);return false;"><?php echo $item->post_text; ?></div>
+														<div class="click-expanding" onclick="javascript:descExpanding('<?php echo $item->post_id; ?>')">
+															<div class="breaker"></div>
+															<div class="inner"><i class="fa fa-plus"></i> Mở rộng</div>
+														</div>
+														<div class="meta-wrap">
+															<ul class="meta">
+																<?php
+																if ( ! empty( $additional->likes ) ) {
+																	?>
+																	<li>
+																		<i class="fa fa-thumbs-up"></i><strong><?php echo $additional->likes; ?></strong>
+																	</li>
+																	<?php
+																}
+																if ( ! empty( $additional->comments ) ) {
+																	?>
+																	<li>
+																		<i class="fa fa-comments"></i><strong><?php echo $additional->comments; ?></strong>
+																	</li>
+																	<?php
+																}
+																if ( ! empty( $additional->shares ) ) {
+																	?>
+																	<li>
+																		<i class="fa fa-share-alt"></i><strong><?php echo $additional->shares; ?></strong>
+																	</li>
+																	<?php
+																}
+																?>
+															</ul>
+														</div>
+														
+														<div class="clr">&nbsp;</div>
+														
+														<div class="bottom" onclick="clickToShow('<?php echo $item->post_id; ?>', <?php echo trim($it['cat_id']); ?>);return false;">
+															<div class="shop">
+																<a href="<?php echo $item->user_link; ?>" class="img" target="_blank">
+																	<img src="<?php echo $item->user_pic; ?>" alt="<?php echo $item->user_screenname; ?>">
+																</a>
+																<div class="shortInfo">
+																	<a href="<?php echo $item->user_link; ?>" target="_blank"><?php echo $item->user_screenname; ?></a>
+																	<p class="date">
+																		<a href="<?php echo $item->post_permalink; ?>" target="_blank"><?php echo date( 'd/m/Y',
+																				$item->post_timestamp ) ?></a></p>
+																</div>
+																<div class="lf-wishlist">
+																	<a href="javascript:;" class="wishlist">
+																		<div class="img-wishlist"></div>
+																		<span class="count-wishlist"><?php echo $item->wishlist; ?></span> </a>
+																</div>
+															</div>
+														</div>
+													</div>
+												</div>
+											<?php endforeach;?>
+										<?php endif;?>
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
+					<?php endif;?>
 
 					<div class="row">
 						<div class="col-xs-12">
--- page-saved-posts.php
+++ page-saved-posts.php
@@ -35,146 +35,123 @@ get_header();
 				<div class="row">
 					<main id="main" class="site-main col-xs-12">
 						<?php 
-							$user_id = get_current_user_id();
-							$path = $_SERVER['DOCUMENT_ROOT'] . "/tmp/" . $user_id . ".user";
-							$arr  = json_decode( file_get_contents( $path ), true );
-							// var_dump($arr[0]);
-							//echo( '<h1>Mẫu quảng cáo đã lưu</h1>' );
-							if ( count( $arr) == 0 ):
-								$arr = array();
-							else:?>
-								<div class="mansory-grid"><div class="grid-sizer"></div><div class="category-post">
-								<?php foreach( $arr as $it):?>
-									<?php
-									// $additional = json_decode( $item->post_additional'] );
-									global $wpdb; $result = $wpdb->get_results( 'SELECT * FROM ' . 'ltt_ff_posts_' . trim($it['cat_id']) . ' WHERE post_id = \'' . $it['post_id'] . '\'', OBJECT );
-									$item = $result[0];
-									$interactive = json_decode($item->post_additional); 
-									?>
-									<div class="item-featured" id="<?php echo trim( $item->post_id ); ?>">
-										<div class="wrap">
-											<div class="avatar">
+						$user_id = get_current_user_id();
+						$path = $_SERVER['DOCUMENT_ROOT'] . "/tmp/" . $user_id . ".user";
+						$arr  = json_decode( file_get_contents( $path ), true );
+						// var_dump($arr[0]);
+						//echo( '<h1>Mẫu quảng cáo đã lưu</h1>' );
+						if ( count( $arr) == 0 ):
+							$arr = array();
+						else:?>
+							<div class="mansory-grid"><div class="grid-sizer"></div><div class="category-post">
+							<?php foreach( $arr as $it):?>
+								<?php
+								// $additional = json_decode( $item->post_additional'] );
+								global $wpdb; $result = $wpdb->get_results( 'SELECT * FROM ' . 'ltt_ff_posts_' . trim($it['cat_id']) . ' WHERE post_id = \'' . $it['post_id'] . '\'', OBJECT );
+								$item = $result[0];
+								$interactive = json_decode($item->post_additional); 
+								?>
+								<div class="item-featured" id="<?php echo trim( $item->post_id ); ?>">
+									<div class="wrap">
+										<div class="avatar">
+											<?php
+											if ( $item->media_type == "video" ) {
+												?>
+												<img src="<?php echo $item->image_url; ?>" alt="" onclick="clickToShow('<?php echo $item->post_id; ?>');return false;">
+												<img class='pview_video' src="<?php echo SITE_URL; ?>wp-content/themes/flatsome/bulkff/assets/images/play.png">
 												<?php
-												if ( $item->media_type == "video" ) {
+											} else {
+												if ( ! empty( $item->image_url ) ) {
 													?>
-													<img src="<?php echo $item->image_url; ?>" alt="" onclick="clickToShow('<?php echo $item->post_id; ?>');return false;">
-													<img class='pview_video' src="<?php echo SITE_URL; ?>wp-content/themes/flatsome/bulkff/assets/images/play.png">
+													<div class="thumb-img">
+														<img src="<?php echo $item->image_url; ?>" alt="" onclick="clickToShow('<?php echo $item->post_id; ?>');return false;">
+														<?php
+														$medias = explode( ",", $item->medias );
+														if ( count( $medias ) > 1 && false) {
+															?>
+															<div class="slider">
+																<div class="owl-carousel owl-theme">
+																	<?php
+																	foreach ( $medias as $img ) {
+																		?>
+																		<a><img aconclick="im_img(this);" src='<?php echo $img; ?>'></a>
+																		<?php
+																	}
+																	?>
+																</div>
+															</div>
+															<?php
+														}
+														?>
+													</div>
 													<?php
 												} else {
-													if ( ! empty( $item->image_url ) ) {
-														?>
-														<div class="thumb-img">
-															<img src="<?php echo $item->image_url; ?>" alt="" onclick="clickToShow('<?php echo $item->post_id; ?>');return false;">
-															<?php
-															$medias = explode( ",", $item->medias );
-															if ( count( $medias ) > 1 && false) {
-																?>
-																<div class="slider">
-																	<div class="owl-carousel owl-theme">
-																		<?php
-																		foreach ( $medias as $img ) {
-																			?>
-																			<a><img aconclick="im_img(this);" src='<?php echo $img; ?>'></a>
-																			<?php
-																		}
-																		?>
-																	</div>
-																</div>
-																<?php
-															}
-															?>
-														</div>
-														<?php
-													} else {
-														?>
-														<div class="thumb-img">
-															<img src="/wp-content/themes/flatsome/assets/img/placeholder-image.jpg" alt="" onclick="clickToShow('<?php echo $item->post_id; ?>');return false;">
-														</div>
-														<?php
-													}
+													?>
+													<div class="thumb-img">
+														<img src="/wp-content/themes/flatsome/assets/img/placeholder-image.jpg" alt="" onclick="clickToShow('<?php echo $item->post_id; ?>');return false;">
+													</div>
+													<?php
+												}
+											}
+											?>
+										</div>
+										<div class="desc" onclick="clickToShow('<?php echo $item->post_id; ?>', <?php echo trim($it['cat_id']); ?>);return false;"><?php echo $item->post_text; ?></div>
+										<div class="click-expanding" onclick="javascript:descExpanding('<?php echo $item->post_id; ?>')">
+											<div class="breaker"></div>
+											<div class="inner"><i class="fa fa-plus"></i> Mở rộng</div>
+										</div>
+										<div class="meta-wrap">
+											<ul class="meta">
+												<?php
+												if ( ! empty( $additional->likes ) ) {
+													?>
+													<li>
+														<i class="fa fa-thumbs-up"></i><strong><?php echo $additional->likes; ?></strong>
+													</li>
+													<?php
+												}
+												if ( ! empty( $additional->comments ) ) {
+													?>
+													<li>
+														<i class="fa fa-comments"></i><strong><?php echo $additional->comments; ?></strong>
+													</li>
+													<?php
+												}
+												if ( ! empty( $additional->shares ) ) {
+													?>
+													<li>
+														<i class="fa fa-share-alt"></i><strong><?php echo $additional->shares; ?></strong>
+													</li>
+													<?php
 												}
 												?>
-											</div>
-											<div class="desc" onclick="clickToShow('<?php echo $item->post_id; ?>', <?php echo trim($it['cat_id']); ?>);return false;"><?php echo $item->post_text; ?></div>
-											<div class="click-expanding" onclick="javascript:descExpanding('<?php echo $item->post_id; ?>')">
-												<div class="breaker"></div>
-												<div class="inner"><i class="fa fa-plus"></i> Mở rộng</div>
-											</div>
-											<div class="meta-wrap">
-												<ul class="meta">
-													<?php
-													if ( ! empty( $additional->likes ) ) {
-														?>
-														<li>
-															<i class="fa fa-thumbs-up"></i><strong><?php echo $additional->likes; ?></strong>
-														</li>
-														<?php
-													}
-													if ( ! empty( $additional->comments ) ) {
-														?>
-														<li>
-															<i class="fa fa-comments"></i><strong><?php echo $additional->comments; ?></strong>
-														</li>
-														<?php
-													}
-													if ( ! empty( $additional->shares ) ) {
-														?>
-														<li>
-															<i class="fa fa-share-alt"></i><strong><?php echo $additional->shares; ?></strong>
-														</li>
-														<?php
-													}
-													?>
-												</ul>
-											</div>
-											
-											<div class="clr">&nbsp;</div>
-											
-											<div class="bottom" onclick="clickToShow('<?php echo $item->post_id; ?>', <?php echo trim($it['cat_id']); ?>);return false;">
-												<div class="shop">
-													<a href="<?php echo $item->user_link; ?>" class="img" target="_blank">
-														<img src="<?php echo $item->user_pic; ?>" alt="<?php echo $item->user_screenname; ?>">
-													</a>
-													<div class="shortInfo">
-														<a href="<?php echo $item->user_link; ?>" target="_blank"><?php echo $item->user_screenname; ?></a>
-														<p class="date">
-															<a href="<?php echo $item->post_permalink; ?>" target="_blank"><?php echo date( 'd/m/Y',
-																	$item->post_timestamp ) ?></a></p>
-													</div>
-													<div class="lf-wishlist">
-														<a href="javascript:;" class="wishlist">
-															<div class="img-wishlist"></div>
-															<span class="count-wishlist"><?php echo $item->wishlist; ?></span> </a>
-													</div>
+											</ul>
+										</div>
+										
+										<div class="clr">&nbsp;</div>
+										
+										<div class="bottom" onclick="clickToShow('<?php echo $item->post_id; ?>', <?php echo trim($it['cat_id']); ?>);return false;">
+											<div class="shop">
+												<a href="<?php echo $item->user_link; ?>" class="img" target="_blank">
+													<img src="<?php echo $item->user_pic; ?>" alt="<?php echo $item->user_screenname; ?>">
+												</a>
+												<div class="shortInfo">
+													<a href="<?php echo $item->user_link; ?>" target="_blank"><?php echo $item->user_screenname; ?></a>
+													<p class="date">
+														<a href="<?php echo $item->post_permalink; ?>" target="_blank"><?php echo date( 'd/m/Y',
+																$item->post_timestamp ) ?></a></p>
+												</div>
+												<div class="lf-wishlist">
+													<a href="javascript:;" class="wishlist">
+														<div class="img-wishlist"></div>
+														<span class="count-wishlist"><?php echo $item->wishlist; ?></span> </a>
 												</div>
 											</div>
 										</div>
 									</div>
-								<?php endforeach;?>
-								<?php
-								/*echo('</div></div>');
-								echo( '<table id="saved_posts" cellpadding="10px" cellspacing="0">' );
-								echo( '<tr class="header">' );
-								echo( '<td>STT</td>' );
-								echo( '<td></td>' );
-								echo( '<td>Fanpage</td>' );
-								echo( '<td>Nội dung</td>' );
-								echo( '</tr>' );
-								$no = 1; foreach( $arr as $item) {
-									global $wpdb; $result = $wpdb->get_results( 'SELECT * FROM ' . 'ltt_ff_posts_' . trim($item->cat_id']) . ' WHERE post_id = \'' . $item->post_id'] . '\'', OBJECT );
-									$interactive = json_decode($result[0]->post_additional); 
-									//var_dump($result);
-									echo( '<tr class="' . ( $no % 2 == 0 ? 'even' : 'odd' ) . '">' );
-									echo( '<td>' . $no . '</td>' );
-									//echo( '<td><a target="_blank" href="' . str_replace(' ', '', $result[0]->post_permalink) . '"><img src="' . str_replace(' ', '', $result[0]->image_url) . '" width="100px"></a></td>' );
-									echo( '<td><a class="avatar-image" target="_blank" href="' . str_replace(' ', '', $result[0]->user_link) . '"><img src="' . str_replace(' ', '', $result[0]->user_pic) . '" /></a></td>' );
-									echo( '<td class="fanpage"><a href="' . str_replace(' ', '', $result[0]->user_link) . '">' . $result[0]->user_screenname . '</a><div class="clr h1"></div><i class="fa fa-thumbs-up"></i> ' . $interactive->likes . ' <i class="fa fa-comments"></i> ' . $interactive->comments . ' <i class="fa fa-share-alt"></i> ' . $interactive->shares . ' <i class="fa fa-eye"></i> ' .$result[0]->views . '</td>' );
-									echo( '<td><div class="saved-post-body">' . $result[0]->post_text . '</div></td>' );
-									echo( '</tr>' );
-									$no++;
-								}
-								echo( '</table>' );*/?>
-							<?php endif;?>
+								</div>
+							<?php endforeach;?>
+						<?php endif;?>
 
 					</main><!-- #main -->
 				</row>
