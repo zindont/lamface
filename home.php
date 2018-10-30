ab74e9fea08205b3aaa5ddd125776198e87433c0
Ân <info@zindo.info>
2018-10-30 10:44:47 +0700

    Print widget system history on home

--- home.php
+++ home.php
@@ -323,90 +323,14 @@ get_header();
 								</div>
 							</div>
 							<div class="col-xs-12">
-								<div class="box box-success">
-									<div class="box-header with-border">
-										<h3 class="box-title">Nhật ký hệ thống</h3>
-
-										<div class="box-tools pull-right">
-											<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-											</button>
-											<button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i>
-											</button>
-										</div>
-									</div>
-									<!-- /.box-header -->
-									<div class="box-body">
-										<!-- Conversations are loaded here -->
-										<div class="direct-chat-messages">
-											<!-- Message. Default to the left -->
-											<div class="direct-chat-msg">
-												<div class="direct-chat-info clearfix">
-													<span class="direct-chat-name pull-left">Alexander Pierce</span>
-													<span class="direct-chat-timestamp pull-right">23 Jan 2:00 pm</span>
-												</div>
-												<!-- /.direct-chat-info -->
-												<img class="direct-chat-img" src="dist/img/user1-128x128.jpg" alt="message user image">
-												<!-- /.direct-chat-img -->
-												<div class="direct-chat-text">
-													Is this template really for free? That's unbelievable!
-												</div>
-												<!-- /.direct-chat-text -->
-											</div>
-											<!-- /.direct-chat-msg -->
-
-											<!-- Message to the right -->
-											<div class="direct-chat-msg right">
-												<div class="direct-chat-info clearfix">
-													<span class="direct-chat-name pull-right">Sarah Bullock</span>
-													<span class="direct-chat-timestamp pull-left">23 Jan 2:05 pm</span>
-												</div>
-												<!-- /.direct-chat-info -->
-												<img class="direct-chat-img" src="dist/img/user3-128x128.jpg" alt="message user image">
-												<!-- /.direct-chat-img -->
-												<div class="direct-chat-text">
-													You better believe it!
-												</div>
-												<!-- /.direct-chat-text -->
-											</div>
-											<!-- /.direct-chat-msg -->
-
-											<!-- Message. Default to the left -->
-											<div class="direct-chat-msg">
-												<div class="direct-chat-info clearfix">
-													<span class="direct-chat-name pull-left">Alexander Pierce</span>
-													<span class="direct-chat-timestamp pull-right">23 Jan 5:37 pm</span>
-												</div>
-												<!-- /.direct-chat-info -->
-												<img class="direct-chat-img" src="dist/img/user1-128x128.jpg" alt="message user image">
-												<!-- /.direct-chat-img -->
-												<div class="direct-chat-text">
-													Working with AdminLTE on a great new app! Wanna join?
-												</div>
-												<!-- /.direct-chat-text -->
-											</div>
-											<!-- /.direct-chat-msg -->
-
-											<!-- Message to the right -->
-											<div class="direct-chat-msg right">
-												<div class="direct-chat-info clearfix">
-													<span class="direct-chat-name pull-right">Sarah Bullock</span>
-													<span class="direct-chat-timestamp pull-left">23 Jan 6:10 pm</span>
-												</div>
-												<!-- /.direct-chat-info -->
-												<img class="direct-chat-img" src="dist/img/user3-128x128.jpg" alt="message user image">
-												<!-- /.direct-chat-img -->
-												<div class="direct-chat-text">
-													I would love to.
-												</div>
-												<!-- /.direct-chat-text -->
-											</div>
-											<!-- /.direct-chat-msg -->
-
-										</div>
-										<!--/.direct-chat-messages-->
-									</div>
-									<!-- /.box-body -->
-								</div>
+								<!-- Codestar_Lamface: Nhật ký hệ thống -->
+								<?php the_widget( 
+										'Codestar_Lamface_System_Histories_Widgets', 
+										array( 
+											'title' => 'Nhật ký hệ thống',
+											'numOfRows' => 3
+										)
+									); ?>
 							</div>
 							<!--/.box -->
 						</div>
