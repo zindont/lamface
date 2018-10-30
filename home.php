74f7a04a29b911f52f38398bf2fd33fa3b27da65
Thanh Hoang <thanhhoang@Thanhs-MacBook-Pro.local>
2018-10-30 12:14:13 +0700

    homepage changes: update customer summary part - 1

--- home.php
+++ home.php
@@ -98,84 +98,106 @@ get_header();
 								</div>
 								<div class="box-body">
 									<div class="row">
-										<div class="col-xs-12 col-md-9">
+										<div class="col-xs-12 col-md-8">
 											<input type="hidden" value="0" name="page" />
 											<div class="mansory-grid">
 												<div class="grid-sizer"></div>
 												<?php echo do_shortcode('[lf_category_post id="13" title="MẪU QUẢNG CÁO MỚI NHẤT" page="5" ]'); ?>
 											</div>
 										</div>
-										<div class="col-xs-12 col-md-3">
-											<!-- Info Boxes Style 2 -->
-											<div class="info-box bg-yellow">
-												<span class="info-box-icon"><i class="ion ion-ios-pricetag-outline"></i></span>
+										<?php if(is_user_logged_in()):?>
+											<div class="col-xs-12 col-md-4">
+												<p class="text-center">
+													<strong>Thống kê của bạn - Gói: Basic </strong>
+												</p>
+												<!-- Info Boxes Style 2 -->
+												<div class="info-box bg-yellow">
+													<span class="info-box-icon"><i class="ion ion-ios-pricetag-outline"></i></span>
 
-												<div class="info-box-content">
-													<span class="info-box-text">Inventory</span>
-													<span class="info-box-number">5,200</span>
+													<div class="info-box-content">
+														<span class="info-box-text">FANPAGE ĐÃ LƯU</span>
+														<span class="info-box-number">5,200</span>
 
-													<div class="progress">
-														<div class="progress-bar" style="width: 50%"></div>
+														<div class="progress">
+															<div class="progress-bar" style="width: 50%"></div>
+														</div>
+														<span class="progress-description">
+															Trong tổng số 30 trang
+														</span>
 													</div>
-													<span class="progress-description">
-																50% Increase in 30 Days
-															</span>
+													<!-- /.info-box-content -->
 												</div>
-												<!-- /.info-box-content -->
-											</div>
-											<!-- /.info-box -->
-											<div class="info-box bg-green">
-												<span class="info-box-icon"><i class="ion ion-ios-heart-outline"></i></span>
+												<!-- /.info-box -->
+												<div class="info-box bg-green">
+													<span class="info-box-icon"><i class="ion ion-ios-heart-outline"></i></span>
 
-												<div class="info-box-content">
-													<span class="info-box-text">Mentions</span>
-													<span class="info-box-number">92,050</span>
+													<div class="info-box-content">
+														<span class="info-box-text">CONTENT ĐÃ LƯU</span>
+														<span class="info-box-number">92,050</span>
 
-													<div class="progress">
-														<div class="progress-bar" style="width: 20%"></div>
+														<div class="progress">
+															<div class="progress-bar" style="width: 20%"></div>
+														</div>
+														<span class="progress-description">
+															Trong tổng số 50 bài
+														</span>
 													</div>
-													<span class="progress-description">
-																20% Increase in 30 Days
-															</span>
+													<!-- /.info-box-content -->
 												</div>
-												<!-- /.info-box-content -->
-											</div>
-											<!-- /.info-box -->
-											<div class="info-box bg-red">
-												<span class="info-box-icon"><i class="ion ion-ios-cloud-download-outline"></i></span>
+												<!-- /.info-box -->
+												<div class="info-box bg-red">
+													<span class="info-box-icon"><i class="ion ion-ios-cloud-download-outline"></i></span>
 
-												<div class="info-box-content">
-													<span class="info-box-text">Downloads</span>
-													<span class="info-box-number">114,381</span>
+													<div class="info-box-content">
+														<span class="info-box-text">CONTENT ĐÃ TẠO</span>
+														<span class="info-box-number">114,381</span>
 
-													<div class="progress">
-														<div class="progress-bar" style="width: 70%"></div>
+														<div class="progress">
+															<div class="progress-bar" style="width: 70%"></div>
+														</div>
+														<span class="progress-description">
+															Trong tổng số 20 bài
+														</span>
 													</div>
-													<span class="progress-description">
-																70% Increase in 30 Days
-															</span>
+													<!-- /.info-box-content -->
 												</div>
-												<!-- /.info-box-content -->
-											</div>
-											<!-- /.info-box -->
-											<div class="info-box bg-aqua">
-												<span class="info-box-icon"><i class="ion-ios-chatbubble-outline"></i></span>
+												<!-- /.info-box -->
+												<div class="info-box bg-aqua">
+													<span class="info-box-icon"><i class="ion-ios-chatbubble-outline"></i></span>
 
-												<div class="info-box-content">
-													<span class="info-box-text">Direct Messages</span>
-													<span class="info-box-number">163,921</span>
+													<div class="info-box-content">
+														<span class="info-box-text">FANPAGE NHẬP VÀO HỆ THỐNG</span>
+														<span class="info-box-number">163,921</span>
 
-													<div class="progress">
-														<div class="progress-bar" style="width: 40%"></div>
+														<div class="progress">
+															<div class="progress-bar" style="width: 40%"></div>
+														</div>
+														<span class="progress-description">
+															Trong tổng số 20 trang
+														</span>
 													</div>
-													<span class="progress-description">
-																40% Increase in 30 Days
-															</span>
+													<!-- /.info-box-content -->
 												</div>
-												<!-- /.info-box-content -->
+												<!-- /.info-box -->
+												<div class="info-box bg-black disabled">
+													<span class="info-box-icon"><i class="ion-ios-chatbubble-outline"></i></span>
+
+													<div class="info-box-content">
+														<span class="info-box-text">CONTENT ĐÃ BÁN</span>
+														<span class="info-box-number">163,921</span>
+
+														<div class="progress">
+															<div class="progress-bar" style="width: 40%"></div>
+														</div>
+														<span class="progress-description">
+															Trong tổng số 20 bài
+														</span>
+													</div>
+													<!-- /.info-box-content -->
+												</div>
+												<!-- /.info-box -->
 											</div>
-											<!-- /.info-box -->
-										</div>
+										<?php endif;?>
 									</div>
 								</div>
 							</div>
@@ -183,7 +205,7 @@ get_header();
 					</div>
 
 					<div class="row">
-						<div class="col-xs-12 col-md-9">
+						<div class="col-xs-12 col-md-8">
 							<div class="col-xs-12">
 								<div class="box box-success">
 									<div class="box-header with-border">
@@ -284,7 +306,7 @@ get_header();
 								</div>
 							</div>
 						</div>
-            			<div class="col-md-3">
+            			<div class="col-xs-12 col-md-4">
 							<!-- USERS LIST -->
 							<div class="col-xs-12">
 								<div class="box box-danger">
