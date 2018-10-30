cc9f0cb9db34cefb50070e9456f8197b11b0c30a
Ân <info@zindo.info>
2018-10-30 11:22:02 +0700

    Add body template for widget

--- inc/codestar/templates/widgets/widget-system_histories.php
+++ inc/codestar/templates/widgets/widget-system_histories.php
@@ -14,18 +14,39 @@
 	// echo esc_html__( 'Hello, World!', 'text_domain' );
 	// echo $args['after_widget'];
 ?>
-<div class="col-xs-12">
-	<div class="box box-success">
-		<div class="box-header with-border">
-			<h3 class="box-title"><?php echo apply_filters( 'widget_title', $instance['title'] ); ?></h3>
 
-			<div class="box-tools pull-right">
-				<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-				</button>
-				<button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i>
-				</button>
-			</div>
+<div class="box box-success">
+	<div class="box-header with-border">
+		<h3 class="box-title"><?php echo apply_filters( 'widget_title', $instance['title'] ); ?></h3>
+
+		<div class="box-tools pull-right">
+			<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+			</button>
+			<button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i>
+			</button>
 		</div>
-		<!-- /.box-header -->
 	</div>
+	<!-- /.box-header -->
+	<div class="box-body">
+		<!-- Conversations are loaded here -->
+		<div class="direct-chat-messages">
+			<!-- Message. Default to the left -->
+			<div class="direct-chat-msg">
+				<div class="direct-chat-info clearfix">
+					<span class="direct-chat-name pull-left">Alexander Pierce</span>
+					<span class="direct-chat-timestamp pull-right">23 Jan 2:00 pm</span>
+				</div>
+				<!-- /.direct-chat-info -->
+				<img class="direct-chat-img" src="/img/broken.png" alt="message user image">
+				<!-- /.direct-chat-img -->
+				<div class="direct-chat-text">
+					Is this template really for free? That's unbelievable!
+				</div>
+				<!-- /.direct-chat-text -->
+			</div>
+			<!-- /.direct-chat-msg -->
+		</div>
+		<!--/.direct-chat-messages-->
+	</div>
+	<!-- /.box-body -->	
 </div>
