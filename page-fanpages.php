cd90eaf02828cad0c898343e5c6f91c7f659da21
tinh <tinh@wiserobot.com>
2018-10-31 19:32:10 +0700

    Update page-fanpages.php

--- page-fanpages.php
+++ page-fanpages.php
@@ -88,7 +88,7 @@ var_dump($show_to);
 									<div class="pagination-wrap">
 										<ul class="pagination" id="pagination-page"></ul>
 										<div class="pagination-wrap-more">
-											<div class="pagination-text">-- Có tất cả <?php echo (count($uniqe_fps)/12); ?> trang --</div>
+											<div class="pagination-text">-- Có tất cả <?php echo ceil(count($uniqe_fps)/12); ?> trang --</div>
 										</div>
 									</div>
 									<!-- /.pagination -->
@@ -98,7 +98,7 @@ var_dump($show_to);
 							<script>
 								jQuery(document).ready(function(){
 									jQuery('#pagination-page').twbsPagination({
-										totalPages: <?php echo (count($uniqe_fps)/12);?>,
+										totalPages: <?php echo ceil(count($uniqe_fps)/12);?>,
 										visiblePages: 3,
 										startPage: <?php echo $current_page;?>,
 										first: '<span class="fa fa-angle-double-left"></span>',
