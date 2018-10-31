2b99ef733c9ead864a0788731c743880b082639c
Thanh Hoang <thanhhoang@Thanhs-MacBook-Pro.local>
2018-10-31 10:52:40 +0700

    update filter formart in archive page - 6

--- filter.php
+++ filter.php
@@ -3,7 +3,7 @@
 <script type="text/javascript" src="<?php echo BULKFF_ASSETS; ?>/js/moment.min.js"></script>
 <link rel="stylesheet" type="text/css" href="<?php echo BULKFF_ASSETS; ?>/css/daterangepicker.css">
 <script src="<?php echo BULKFF_ASSETS; ?>/js/isotope.pkgd.min.js"></script>
-<script type="text/javascript" src="<?php echo get_template_directory() ; ?>/assets/js/select2.full.min.js"></script>
+<script type="text/javascript" src="<?php echo get_template_directory_uri() ; ?>/assets/js/select2.full.min.js"></script>
 <script type="text/javascript">
     jQuery(document).ready(function () {
         jQuery('.select2').select2();
