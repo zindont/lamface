dfc1a6c44f1c10ed8318a46852a1a5c5eef2ab7d
tinh <tinh@wiserobot.com>
2018-11-01 18:36:21 +0700

    Update home.php

--- home.php
+++ home.php
@@ -103,7 +103,7 @@ $table_cat = 'ltt_ff_cate';
 								</div>
 								<div class="box-body">
 									<div class="row">
-									
+										<?php if(is_user_logged_in()):?>
 											<div class="col-xs-12 col-md-9">
 										<?php else:?>
 											<div class="col-xs-12">
